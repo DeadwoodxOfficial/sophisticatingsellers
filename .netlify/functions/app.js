9c30a4c173dc5918cb58a18450c2525e4e505b0a
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-01-04 12:54:36 +0530

    Update app.js

--- .netlify/functions/app.js
+++ .netlify/functions/app.js
@@ -1,11 +1,13 @@
-const express = require("express")
+const express = require("express");
+const serverless = require("serverless-http");
+
 const app = express();
-const serverless = require("serverless-http")
 const router = express.Router();
 
-router.get("/",(req,res) =>{
-  res.json("ok")
-})
+router.get("/", (req, res) => {
+  res.json({ message: "OK" });
+});
 
-app.use("/.netlify/functions/app",router);
-module.exports.handler = serverless(app)
+app.use("/.netlify/functions/app", router);
+
+module.exports.handler = serverless(app);
