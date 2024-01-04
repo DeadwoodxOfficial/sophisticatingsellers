32594de7151430fb05153b199bbca361c3e4a491
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-01-04 12:42:43 +0530

    k

--- .netlify/functions/app.js
+++ .netlify/functions/app.js
@@ -1,29 +1,11 @@
-// app.js (inside the Netlify functions directory)
+const express = require("express")
+const app = express();
+const serverless = require("serverless-http")
+const router = express.Router();
 
-// Define the getUser function
-exports.getUser = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    return {
-      statusCode: 405,
-      body: 'Only GET requests are allowed for this endpoint.',
-    };
-  }
+router.get("/",(req,res) =>{
+  res.json("ok")
+})
 
-  try {
-    // Your getUser logic here
-    // Access request data using event.body, event.queryStringParameters, etc.
-    // Perform actions like fetching user data from a database or other operations
-
-    const userData = { id: 1, username: 'john_doe' }; // Example user data
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(userData),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
-    };
-  }
-};
+app.use("/.netlify/functions/app",router);
+module.exports.handler = serverless(app)
